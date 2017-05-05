82c7ac85b9dd2a42ed6f32a8f48b071ad596a345
zaus <dr.zaus+dev@gmail.com>
2017-05-05 04:49:52 -0400

    trying to harden the test page -- encapsulating and visitor checking

--- test-response.php
+++ test-response.php
@@ -1,74 +1,105 @@
 <?php
 
-$type = $_SERVER["CONTENT_TYPE"];
-if(empty($type)) {
-	$headers = getallheaders();
-	if(isset($headers['Accept'])) $type = $headers['Accept'];
-	if(empty($type) && isset($headers['Content-Type'])) $type = $headers['Content-Type'];
-}
+class F3pInjectTester {
 
-// echo $type, "\n";
-// alter keys so it will look different for echo
-$prerequest = $_POST + $_GET;
+	public function __construct() {
+		$request = $this->test($_SERVER, $_POST, $_GET, file_get_contents("php://input"));
 
-$raw = file_get_contents("php://input");
-
-if(strpos($type, 'json') !== false) {
-	// also attach decoded raw body
-	$prerequest += json_decode($raw, true);
-}
-// probably do the same for xml, but too much trouble
-
-$request = array();
-// dynamically modify the 'request'
-$dyn = array('prefix' => null, 'suffix' => null);
-foreach($dyn as $k => &$o)
-	if(isset($prerequest[$k])) {
-		$o = $prerequest[$k];
-		unset($prerequest[$k]);
+		// if it gets this far
+		plain_print($request);
 	}
 
-foreach($prerequest as $k => $v) {
-	$request[ 'req-' . $k ] = $dyn['prefix'] . $v . $dyn['suffix'];
-}
+	public function get_headers() {
+		return getallheaders();
+	}
 
-// print_r(array('req' => $req, 'raw' => $raw));
+	public function test($_SERVER, $_POST, $_GET, $raw) {
 
-if(strpos($type, 'json') !== false) {
-	echo json_encode($request);
-	exit;
-}
-if(strpos($type, 'xml') !== false) {
-	// http://stackoverflow.com/questions/1397036/how-to-convert-array-to-simplexml
-	function to_xml(SimpleXMLElement $object, array $data)
-	{
-		foreach ($data as $key => $value) {
-			if (is_array($value)) {
-				$new_object = $object->addChild($key);
-				to_xml($new_object, $value);
-			} else {
-				$object->addChild($key, $value);
+		$type = $_SERVER["CONTENT_TYPE"];
+		if(empty($type)) {
+			$headers = $this->get_headers();
+			if(isset($headers['Accept'])) $type = $headers['Accept'];
+			if(empty($type) && isset($headers['Content-Type'])) $type = $headers['Content-Type'];
+		}
+
+		// echo $type, "\n";
+		// alter keys so it will look different for echo
+		$prerequest = $_POST + $_GET;
+
+		if(strpos($type, 'json') !== false) {
+			// also attach decoded raw body
+			$prerequest += json_decode($raw, true);
+		}
+		// probably do the same for xml, but too much trouble
+
+		$request = array();
+		// dynamically modify the 'request'
+		$dyn = array('prefix' => null, 'suffix' => null);
+		foreach($dyn as $k => &$o)
+			if(isset($prerequest[$k])) {
+				$o = $prerequest[$k];
+				unset($prerequest[$k]);
+			}
+
+		foreach($prerequest as $k => $v) {
+			$request[ 'req-' . $k ] = $dyn['prefix'] . $v . $dyn['suffix'];
+		}
+
+		// print_r(array('req' => $req, 'raw' => $raw));
+
+		if(strpos($type, 'json') !== false) {
+			echo json_encode($request);
+			exit;
+		}
+		if(strpos($type, 'xml') !== false) {
+			// http://stackoverflow.com/questions/1397036/how-to-convert-array-to-simplexml
+			function to_xml(SimpleXMLElement $object, array $data)
+			{
+				foreach ($data as $key => $value) {
+					if (is_array($value)) {
+						$new_object = $object->addChild($key);
+						to_xml($new_object, $value);
+					} else {
+						$object->addChild($key, $value);
+					}
+				}
+			}
+
+			if(empty($raw)) $raw = '<request />';
+			echo to_xml(new SimpleXMLElement($raw), $request)->asXML();
+			exit;
+		}
+
+		// maybe it gets this far
+		return $request;
+	}
+
+
+	public function plain_print($request, $depth = 0) {
+		// add some whitespace just to make sure trim works
+
+		if(!is_array($request) && !is_object($request)) echo $request;
+		else {
+			$indent = str_repeat("\t", $depth);
+
+			foreach($request as $k => $v) {
+				echo "\n", $indent, $k, ' = ';
+				plain_print($v, $depth+1);
 			}
 		}
 	}
+}
 
-	if(empty($raw)) $raw = '<request />';
-	echo to_xml(new SimpleXMLElement($raw), $request)->asXML();
+
+// protect
+
+// same ip?
+$visitor = $_SERVER['REMOTE_ADDR'];
+$me = $_SERVER['REMOTE_ADDR'];
+
+if($visitor == $me) new F3iGfResend();
+else {
+	echo json_encode(array('me' => $me, 'visitor' => $visitor, 'error' => 'rejected'));
 	exit;
 }
-
-function plain_print($request, $depth = 0) {
-	// add some whitespace just to make sure trim works
-
-	if(!is_array($request) && !is_object($request)) echo $request;
-	else {
-		$indent = str_repeat("\t", $depth);
-
-		foreach($request as $k => $v) {
-			echo "\n", $indent, $k, ' = ';
-			plain_print($v, $depth+1);
-		}
-	}
-}
-plain_print($request);
 
